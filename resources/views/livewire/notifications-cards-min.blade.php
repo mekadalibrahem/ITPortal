849db5c2e33cc9e0fd20099b5fd7c0d62af9c3ca
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2025-01-27 20:27:16 +0300

    edit home design

--- resources/views/livewire/notifications-cards-min.blade.php
+++ resources/views/livewire/notifications-cards-min.blade.php
@@ -1,20 +1,12 @@
-<section class="pt-10 mb-4">
-    <h1 class="mb-5 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {{__("string.notifications")}}
-     </h1>
-
+<x-widgets.section title="{{ __('string.notifications') }}">
     <div class="grid gap-8 lg:grid-cols-3 ">
 
-        @forelse ($notifications as $notification )
-
+        @forelse ($notifications as $notification)
             <livewire:notifications-card :notification="$notification" />
 
         @empty
-         <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md  text-gray-900 dark:text-white dark:bg-gray-800 dark:border-gray-700">
             <p>
-                {{__("string.Empty notifications")}}
+                {{ __('string.Empty notifications') }}
             </p>
-         </div>
         @endforelse
-    </div>
-</section>
+</x-widgets.section>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -3,11 +3,8 @@
         title
     </x-slot:title>
     <main class=" h-screen  dark:bg-gray-700" >
-        <section class="pt-10 mb-4">
-            <h1 class="mb-5 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {{__("string.Current requests")}}
-                </h1>
 
+        <x-widgets.section title="{{__('string.Current requests')}}">
             <div class="grid gap-8 lg:grid-cols-3 ">
 
                 @forelse ($request_list as $request )
@@ -17,14 +14,15 @@
 
 
                 @empty
-                 <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md  text-gray-900 dark:text-white  dark:bg-gray-800 dark:border-gray-700">
+
                     <p>  {{__("text.Empty request")}}  </p>
-                 </div>
+
                 @endforelse
             </div>
-        </section>
+        </x-widgets.section>
 
 
+        
        <livewire:notifications-cards-min  :user_id="$user_id" />
 
 
