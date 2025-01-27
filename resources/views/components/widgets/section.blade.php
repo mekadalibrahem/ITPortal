f5b9b97264a46dec691396e0013680a88617014d
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2025-01-27 20:22:28 +0300

    fix title sextion component

--- resources/views/components/widgets/section.blade.php
+++ resources/views/components/widgets/section.blade.php
@@ -2,7 +2,7 @@
      <section class="bg-white sm:mx-0   md:mx-auto  px-4  mt-8 rounded-lg  dark:bg-gray-900">
         <div class="max-w-2xl px-4 py-8 mx-auto lg:py-16">
             <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">
-               {{ __('string.Change password') }}
+               {{ $title }}
            </h2>
            {{
                 $slot
