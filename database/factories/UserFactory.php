61eac178cd39197ebfb1fa27b3c4a7ed165ce7db
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-02-04 17:58:46 +0300

    add userFacroty

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'fname' => fake()->name(),
+            'mname' => fake()->name(),
+            'lname' => fake()->name(),
+            'username' => fake()->unique()->userName(),
+            'national_id' => fake()->unique()->numerify(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -37,7 +41,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
