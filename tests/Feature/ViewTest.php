13c47ffc880a7999b8d7a20dfed36b545c001467
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-02-05 13:21:24 +0300

    remove unused imported classes

--- tests/Feature/ViewTest.php
+++ tests/Feature/ViewTest.php
@@ -3,9 +3,6 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class ViewTest extends TestCase
