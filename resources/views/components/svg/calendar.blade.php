7f6bad967c31fbec3f2fe934e500c4fa690fbe2e
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-02-07 18:55:55 +0300

    add some icons set

--- resources/views/components/svg/calendar.blade.php
+++ resources/views/components/svg/calendar.blade.php
@@ -1,5 +1,6 @@
 <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    {{ $attributes->merge([ 'class' => 'w-6 h-6 ' ]) }}>
     <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
     <line x1="16" x2="16" y1="2" y2="6" />
     <line x1="8" x2="8" y1="2" y2="6" />
--- resources/views/components/svg/documentation.blade.php
+++ resources/views/components/svg/documentation.blade.php
@@ -1,5 +1,6 @@
 <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    {{ $attributes->merge([ 'class' => 'shrink-0 size-4' ]) }}>
     <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
     <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
 </svg>
