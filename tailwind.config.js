f0a3220626bd9ceda174ab733dbe5232c66191c8
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-03-30 14:45:04 +0300

    enable dark mode options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
+
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'media',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
