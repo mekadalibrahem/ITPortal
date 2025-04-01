643b9e0ef1832e76154d5aaeb6145b5ec4ee1b38
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-04-01 15:29:06 +0300

    fxi min width

--- resources/views/components/switch-lang.blade.php
+++ resources/views/components/switch-lang.blade.php
@@ -1,4 +1,4 @@
-<div class="w-auto" >
+<div class=" min-w-16 w-auto" >
     <form action="{{Route('language.switch')}}" method="POST">
         @csrf
         <select name="language" onchange="this.form.submit()" class="  bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
