4e3aaa6b638d9cdd8bd6022a549850721b20a3ba
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-08-15 00:22:48 +0300

    update  add new request to store all step  in log table when create new request instance

--- app/Traits/AddNewRequestTransaction.php
+++ app/Traits/AddNewRequestTransaction.php
@@ -12,6 +12,7 @@ use App\Models\RequestLog;
 use App\Models\Requests;
 use App\Models\RequestTemplates\OrderStep;
 use App\Models\RequireData;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
@@ -21,24 +22,25 @@ trait AddNewRequestTransaction
     public $is_done = false;
     public $image;
 
-    public function add_new_request_transaction($draft, $user_id, $request_id, $request_data , )
+    public function add_new_request_transaction($draft, $user_id, $request_id, $request_data,)
     {
 
-        DB::transaction(function () use ($draft, $user_id, $request_id, $request_data ) {
+        DB::transaction(function () use ($draft, $user_id, $request_id, $request_data) {
             try {
                 // setp 1 :  insert request list row
-                $status = $draft ? RequestStatusEnum::DRAFT->value : RequestStatusEnum::CHECKING->value;
+                $status = $draft ? RequestStatusEnum::DRAFT->value : RequestStatusEnum::WATING->value;
 
                 $req = Requests::where('id', '=', $request_id)->first();
 
                 $dean = CollageInformations::where('name', '=', 'DEAN')->first();
                 $coor = CollageInformations::where('name', '=', 'COOR_NAME_AR')->first();
-                $firstStep = OrderStep::where('request_template_id' , $req->request_template_id)->orderBy('order')->get()->first();
-                $new_request =  RequestList::create([                    
+                $templateSteps = OrderStep::where('request_template_id', $req->request_template_id)->orderBy('order')->get();
+                $firstStep = $templateSteps->first();
+                $new_request =  RequestList::create([
                     "user_id" => $user_id,
                     "request_template_id" => $req->request_template_id,
                     "status"  => $status,
-                    "current_step_id" =>$firstStep->request_tamplates_steps_id ,
+                    "current_step_id" => $firstStep->request_tamplates_steps_id,
                     "request_id" => $request_id,
                     "dean" => $dean->value,
                     "coordinator" => $coor->value
@@ -61,14 +63,26 @@ trait AddNewRequestTransaction
                         $this->image->storeAs("request_photos", $file_name, 'request');
                         $value = $file_name;
                     }
-                    
+
                     $data = Data::create([
                         'name' => $key,
                         "value" => $value,
                         "request_list_id" => $new_request->id
                     ]);
                 }
-               
+                // create request log for all steps in request 
+                // 1- create array for each step 
+                $now = Carbon::now();
+                $request_log_steps_array =$templateSteps->map(function ($step)  use($new_request,$now){
+                    return [
+                        'request_list_id' => $new_request->id ,
+                        'request_tamplates_step_id' => $step->request_tamplates_steps_id,
+                        'created_at' => $now ,
+                        'updated_at' => $now,
+                    ];
+                })->all();
+                // dd($templateSteps , $request_log_steps_array , $templateSteps->pluck('request_tamplates_steps_id'));
+                DB::table('request_logs')->insert($request_log_steps_array);
                 DB::commit();
                 $this->is_done = true;
             } catch (\Throwable $th) {
