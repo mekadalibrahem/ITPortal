2735fa2c87b0031f1c308d99fce4d5f277c91398
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-08-19 19:53:08 +0300

    merege if statments in one

--- resources/views/livewire/employee/requests-card.blade.php
+++ resources/views/livewire/employee/requests-card.blade.php
@@ -3,9 +3,9 @@
         <livewire:request-card.steps-card :id="$request_id" />
         <livewire:request-card.info-card hasexport="{{ true }}" :request="$request">
 
+            @if ($can_work)
+                <div class="mt-6 space-y-4">
 
-            <div class="mt-6 space-y-4">
-                @if ($can_work)
                     <div class="grid gap-2 md:grid-cols-3">
                         <x-form.label for="cancel_note">{{ __('string.cancel note title') }}</x-form.label>
                         <textarea id="cancel_note" placeholder="{{ __('string.placholder_cancel_note') }}" name="cancel_note"
@@ -17,19 +17,19 @@
                             @enderror
                         </div>
                     </div>
-                @endif
-                <div class="grid gap-4 md:grid-cols-4 mt-4">
-                    @if ($can_work)
+
+                    <div class="grid gap-4 md:grid-cols-4 mt-4">
+
                         <x-button status="success" type="button"
                             wire:click="accept()">{{ __('string.accept') }}</x-button>
                         <x-button status="primary" type="button"
                             wire:click="cancel()">{{ __('string.askToedit') }}</x-button>
                         <x-button status="danger " type="button"
                             wire:click="reject()">{{ __('string.reject') }}</x-button>
-                    @endif
 
+
+                    </div>
                 </div>
-            </div>
-
+            @endif
     @endif
 </x-widgets.section>
