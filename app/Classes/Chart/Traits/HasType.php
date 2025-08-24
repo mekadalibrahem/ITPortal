4d280cd7e8af3800a13406030a633bb3d4670aba
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-08-24 20:19:12 +0300

    check in type is valid and throw error if not

--- app/Classes/Chart/Traits/HasType.php
+++ app/Classes/Chart/Traits/HasType.php
@@ -2,12 +2,30 @@
 
 namespace App\Classes\Chart\Traits;
 
+use Exception;
+
 trait HasType
-{
+{   
+    public const CHART_TYPES = [
+         "bar",
+         "horizontalBar",
+         "bubble",
+         "scatter",
+         "doughnut",
+         "line",
+         "pie",
+         "polarArea",
+         "radar",
+    
+    ];
     public $chart_type;
     public function setChartType($type)
     {
-        $this->chart_type = $type;
+        if(in_array($type , self::CHART_TYPES)){
+            $this->chart_type = $type;
+        }else{
+            throw new Exception("INVALID CHART TYPE  avalibale types is {" . implode(' , ' , self::CHART_TYPES) . "}");
+        }
     }
     public function getChartType()
     {
