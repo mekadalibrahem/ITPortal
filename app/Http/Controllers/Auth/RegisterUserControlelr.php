db3636738c95e5ffa70a765a554d2caf1b6c6fcb
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-09-08 20:34:36 +0300

    remove add user from register page  -> add just student

--- app/Http/Controllers/Auth/RegisterUserControlelr.php
+++ app/Http/Controllers/Auth/RegisterUserControlelr.php
@@ -3,21 +3,20 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use App\Classes\Services\Actions\UserAction;
 
 class RegisterUserControlelr extends Controller
 {
     /**
      * show register view
      */
-    public function create():View
+    public function create(): View
     {
         return view('auth.register');
     }
@@ -28,47 +27,32 @@ class RegisterUserControlelr extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'fname' => ['required' , 'string' , 'max:255']  ,
-            'mname' => ['required' , 'string' , 'max:255']  ,
-            'lname' => ['required' , 'string' , 'max:255']  ,
-            'nid' => ['required' , 'string' , 'max:255']  ,
-            "username" =>  ['required' , 'string' , 'max:255' , 'unique:users,username' ]  ,
-            'email' => ['required' , 'string' , 'email' , 'max:255' , 'unique:users,email'],
+            'fname' => ['required', 'string', 'max:255'],
+            'mname' => ['required', 'string', 'max:255'],
+            'lname' => ['required', 'string', 'max:255'],
+            'nid' => ['required', 'string', 'max:255'],
+            "username" =>  ['required', 'string', 'max:255', 'unique:users,username'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
 
-            'password' => ['required' , 'min:8' , 'same:confirm_password'],
-            "type" => ['required' ],
+            'password' => ['required', 'min:8', 'same:confirm_password'],
         ]);
 
-        if($request->type == 1 || $request->type == 2){
-            //  dd($request->type);
-            $user = User::create([
-                'fname'=> $request->fname ,
-                'mname'=> $request->mname ,
-                'lname'=> $request->lname ,
-                'username'=> $request->username ,
-                'email'=> $request->email ,
-                "national_id" => $request->nid ,
-                'password'=> Hash::make($request->password)
-            ]);
+        $user = UserAction::register([
+            'fname' => $request->fname,
+            'mname' => $request->mname,
+            'lname' => $request->lname,
+            'username' => $request->username,
+            'email' => $request->email,
+            "national_id" => $request->nid,
+            'password' => $request->password
+        ], [
+            'student'
+        ]);
 
-            // dd($user);
-            try {
-                if($request->type == 1){
-                    $user->assignRole('student');
-                }elseif($request->type==2){
-                    $user->assignRole('employee_requests');
-                }
-            } catch (\Throwable $th) {
-                Log::warning("Can't find role  for assign to user");
-            }
 
-            event(new Registered($user));
-            Auth::login($user);
+        event(new Registered($user));
+        Auth::login($user);
 
-            return redirect(route('home' ));
-        }else{
-            return redirect()->back();
-        }
-
+        return redirect(route('home'));
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -50,24 +50,6 @@
                             <x-widgets.input id="nid" name="nid" label="{{ __('string.ID number') }}"
                                 value="{{ old('nid') }}" />
 
-                            <div>
-                                <x-form.label for="type" value="{{ __('string.Account Type') }}" />
-                                <select name="type" id="type"
-                                    class='border border-gray-300 text-gray-900 sm:text-sm rounded-lg bg-cyan-50 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 bg-red dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
-                                    <option value="0">
-                                        {{ __('string.select type') }}
-                                    </option>
-                                    <option value="1">
-                                        {{ __('string.student') }}
-                                    </option>
-                                    <option value="2">
-                                        {{ __('string.employee') }}
-                                    </option>
-                                </select>
-
-                                <x-form.input-error :message="$errors->get('test')" />
-
-                            </div>
 
                         </div>
 
