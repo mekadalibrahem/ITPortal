aca74c32ee4ea4279486b40f4b3e3545be707dc6
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-09-12 17:47:28 +0300

    add event  and listner for edit request data

---
+++ app/Listeners/SendRequestListEdited.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\RequestListEdited;
+use App\Models\RequestLog;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendRequestListEdited
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(RequestListEdited $event): void
+    {
+        try {
+            $currentLog = RequestLog::query()->where([
+                'request_list_id' =>  $event->requestList->id,
+                'request_tamplates_step_id' =>  $event->requestList->current_step_id
+            ])->first();
+            $note = "Data updated : \n";
+
+            foreach ($event->dataChanged as $item) {
+                if (!$item['isImage']) {
+                    $note .=  "[ " . $item['key'] . " ] from [" . $item['old'] . '] to [' . $item['new'] . "] \n";
+                } else {
+                    $note .=  "[ " . $item['key'] . " ] image updated \n";
+                }
+            }
+            $note .= "\nby : " . $event->requestList->user->email . " (" . now()->format('Y-m-d H:i:s') . ")\n";
+            $note .= "---------------------------\n";
+            $currentLog->note =  $note . "\n" . $currentLog->note;
+            $currentLog->save();
+        } catch (\Throwable $th) {
+            logger()->error($th->getMessage());
+        }
+    }
+}
--- app/Traits/UpdateRequestTransaction.php
+++ app/Traits/UpdateRequestTransaction.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use App\Enums\DataTypeEnum;
 use App\Enums\RequestStatusEnum;
+use App\Events\RequestListEdited;
 use App\Models\Data;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -14,12 +15,12 @@ trait UpdateRequestTransaction
     public $image;
     public $old_image_path;
     public $is_done = false;
-
+    public $data_changed = [];
     public function update_request_trans($req,  $new_data, $draft)
     {
 
         DB::transaction(function ()  use ($req, $new_data, $draft) {
-              
+
             try {
                 $updated = [];
 
@@ -34,10 +35,17 @@ trait UpdateRequestTransaction
 
                 foreach ($new_data as $key => $value) {
 
-
                     $edited_data = Data::where('request_list_id', '=', $req->id)
                         ->where("name", '=', $key)
                         ->first();
+
+                    $item  = [
+                        'key' => $key,
+                        'isImage' => 0,
+                        'old' => $edited_data->value,
+                        "new" => $value,
+                    ];
+
 
                     if ($edited_data->type() == DataTypeEnum::IMAGE->value) {
                         $this->image = $value;
@@ -48,14 +56,19 @@ trait UpdateRequestTransaction
                         $file_name = $req->user_id . "_" . $req->id . "_" . $edited_data->name . "_" . $time . "." . $extension;
                         $this->image->storeAs("request_photos", $file_name, 'request');;
                         $value = $file_name;
+                        $item['isImage'] = 1;
                     }
 
                     $edited_data->value = $value;
+                    if ($edited_data->isDirty()) {
+                        $this->data_changed[]  = $item;
+                    }
                     $edited_data->save();
                 }
 
 
                 DB::commit();
+                event(new RequestListEdited($req, $this->data_changed));
                 // self::delete_old_image($this->images_for_deleted);
                 $this->is_done = true;
             } catch (\Throwable $th) {
@@ -63,18 +76,17 @@ trait UpdateRequestTransaction
                 // throw new \Exception("Error Processing Request" . $th, 1);
 
                 dd("FALID TRANSACTION UPDATE REQUEST LIST ITEM : $th");
-
             }
-
         });
         return $this->is_done;
     }
 
-    public static function delete_old_image($images){
+    public static function delete_old_image($images)
+    {
         try {
             foreach ($images as $image) {
 
-              Storage::delete("uploads/request_photos/".$image);
+                Storage::delete("uploads/request_photos/" . $image);
             }
         } catch (\Throwable $th) {
             dd($th);
